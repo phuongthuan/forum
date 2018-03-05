19902203063a513e252a6dac991ebf53eef65c17
NgPhuongThuan <nguyenphuongthuan95@gmail.com>
2018-03-05 15:10:10 +0700

    Episode3

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -9,10 +9,28 @@
 
                     <div class="card-body">
                         {{ $thread->body }}
-                            <hr>
+                    <hr>
                     </div>
                 </div>
             </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @foreach($thread->replies as $reply)
+
+                    <div class="card">
+                        <div class="card-header">
+                            <a href="#">
+                                {{ $reply->owner->name }}
+                            </a> said <b>{{ $reply->created_at->diffForHumans() }} ...</b>
+                        </div>
+                        <div class="card-body">
+                            {{ $reply->body }}
+                        </div>
+                    </div>
+                @endforeach
+            </div>git
         </div>
     </div>
 @endsection
