8d0d89bfb6ed1e90819358968695c0699ec44640
NgPhuongThuan <nguyenphuongthuan95@gmail.com>
2018-03-07 13:57:49 +0700

    Validation request

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -29,4 +29,31 @@ class ParticipateInForumTest extends TestCase
         $this->post($thread->path() . '/replies', $reply->toArray());
         $this->get($thread->path())->assertSee($reply->body);
     }
+    
+    /** @test */
+    public function a_reply_requires_a_body()
+    {
+        $this->signIn();
+        $thread = create(Thread::class);
+
+        $reply = make(Reply::class, ['body' => null]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray())
+        ->assertSessionHasErrors('body');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
