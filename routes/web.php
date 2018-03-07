b7abe27da741160c7c115cd6ca7c003436ff2123
NgPhuongThuan <nguyenphuongthuan95@gmail.com>
2018-03-07 15:04:37 +0700

    Add filter Threads

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,4 @@ Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
 Route::post('/threads', 'ThreadsController@store');
 
 Route::post('/threads/{channel}/{thread}/replies', "RepliesController@store");
-
+Route::get('threads/{channel}', "ThreadsController@index");
