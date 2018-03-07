7049e442cd8a7d60edfd441eca5c649245f248f4
NgPhuongThuan <nguyenphuongthuan95@gmail.com>
2018-03-07 18:25:48 +0700

    Episode12 + 13 + 14

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Thread;
 use App\Channel;
+use App\User;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -25,10 +26,19 @@ class ThreadsController extends Controller
     public function index(Channel $channel)
     {
         if ($channel->exists) {
-            $threads = $channel->threads()->latest()->get();
+            $threads = $channel->threads()->latest();
         } else {
-            $threads = Thread::latest()->get();
+            $threads = Thread::latest();
         }
+
+        // if request('by'), we should filter by given username.
+        if ($username = request('by')) {
+            $user = User::where('name', $username)->firstOrFail();
+            $threads = Thread::where('user_id', $user->id);
+        }
+
+        $threads = $threads->get();
+
         return view('threads.index', compact('threads'));
     }
 
