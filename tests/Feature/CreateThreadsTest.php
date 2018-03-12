6233ec0c58967a11a818c9687c1ab2629a2a2b90
NgPhuongThuan <nguyenphuongthuan95@gmail.com>
2018-03-12 15:13:05 +0700

    Fix delete activities

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Activity;
 use App\Channel;
 use App\Reply;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -102,7 +103,8 @@ class CreateThreadsTest extends TestCase
         $this->assertDatabaseMissing('threads', ['id' => $thread->id]);
 
         $this->assertDatabaseMissing('replies', ['id' => $reply->id]);
-    }
 
+        $this->assertEquals(0, Activity::count());
+    }
 
 }
