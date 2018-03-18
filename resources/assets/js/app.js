109468d05b48d2c462e994a976267193373c34be
NgPhuongThuan <nguyenphuongthuan95@gmail.com>
2018-03-18 11:04:17 +0700

    Add Flash Vue message

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,13 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+window.events = new Vue();
+
+window.flash = function(message) {
+    window.events.$emit('flash', message);
+}
+
+Vue.component('flash', require('./components/Flash.vue'));
 
 const app = new Vue({
     el: '#app'
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -8,7 +8,6 @@
             </h6>
             <div>
 
-
                 <form method="POST" action="/replies/{{ $reply->id }}/favorites">
                     {{ csrf_field() }}
                     <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
